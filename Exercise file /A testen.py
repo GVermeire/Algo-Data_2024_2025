d1726741ef720cd0c6a7bcd47e15fee586d6ee1b
Vermeire <132931623+GVermeire@users.noreply.github.com>
2024-11-06 18:31:10 +0100

    update

--- Exercise file /A testen.py
+++ Exercise file /A testen.py
@@ -1,20 +1,21 @@
-def samenvoegen(lst1: list, lst2: list):
-    rits =[]
-    lengte1 = len(lst1)
-    lengte2 = len(lst2)
-    if lengte1 == lengte2:
-        for i in range(lengte1):
-            rits.append(lst1[i])
-            rits.append(lst2[i])
-        return rits
-    elif lengte1 < lengte2:
-        for i in range(lengte1):
-            rits.append(lst1[i])
-            rits.append(lst2[i])
-        return rits
-    else:
-        for i in range(lengte2):
-            rits.append(lst1[i])
-            rits.append(lst2[i])
-        return rits
+def iszigzag(zigzag: list):
+    for i in range(len(zigzag)-1):
+        if zigzag[i] > zigzag[i+1] and zigzag[i+1] < zigzag[i+2]:
+            return True
+        else:
+            return False
 
+# print(iszigzag([2, 1, 10, 5, 49, 23, 90])) #gesorteerde lijst
+# print(iszigzag( [10, 90, 49, 2, 1, 5, 23])) #ongesorteerde lijst
+# True and False
+
+def iszigzag(seq):
+    """Controleert of de gegeven reeks zigzag-gesorteerd is."""
+    for i in range(len(seq) - 1):
+        if i % 2 == 0:  # even index
+            if not (seq[i] >= seq[i + 1]):  # moet groter of gelijk zijn aan volgende
+                return False
+        else:  # oneven index
+            if not (seq[i] <= seq[i + 1]):  # moet kleiner of gelijk zijn aan volgende
+                return False
+    return True
