c4c234dd5138214f4f99113560ed20cc009534ad
Vermeire <132931623+GVermeire@users.noreply.github.com>
2024-11-06 23:45:13 +0100

    update

--- W2 Exceptions, files, Sets, Tulpes, Dicts/Woorden Tellen,.py
+++ W2 Exceptions, files, Sets, Tulpes, Dicts/Woorden Tellen,.py
@@ -1,9 +1,30 @@
 # aangezien elk karakter die geen letter is een scheidingsteken is vragen ze eerst te splitsen
-def woorden_splitsen(bestand_locatie):
+from collections import defaultdict
+
+
+def woorden_splitsen(bestand_locatie: str):
     try:
         # open het bestand en lees de inhoud in
-        with open(bestand_locatie, "r") as bestand: # r = read = lezen, en we open het als bestand, mag eender welke naam zijn
-            tekst = bestand.read()
+        with open(bestand_locatie, "r") as file: # r = read = lezen, en we open het als bestand, mag eender welke naam zijn
+            tekst = file.read()
 
         woorden = tekst.split()
+        print(woorden)
+
+    except FileNotFoundError:
+        print("File not found")
+
+# woorden_splitsen("data.txt")
+
+def woorden_tellen(bestand_locatie: str) -> dict:
+    woorden = woorden_splitsen(bestand_locatie)
+    telling = defaultdict(int)
+    for woord in woorden:
+        woord = woord.lower()
+        telling[woord] += 1
+
+    return dict(telling)
+
+
+
 
